265d2054d87d6f06b35c196c251631ea077e1818
iamShah <formal>
2020-10-08 10:16:30 -0400

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,21 @@
-def say_hello (name = "Ruby Programming")
-    puts "Hello #{name}!"
-  end
+# def say_hello(name = "Ruby Programming")
+#     puts "Hello #{name}!"
+#   end
+# say_hello()
+# def say_hello(name = "Ruby programmer")
+#   puts "Hello, #{name}
+# end
+# say_hello("Gabriela")
+# def say_hello(name="Ruby Programmer!")
+#   puts ("Hello #{name}!")
+# end 
+# say_hello("Kent Beck")
+# def say_hello(name)
+#   puts "Hello Kent Beck"
+#   end
   
-  say_hello ("Gabriela")
+  
+  def say_hello(name="Ruby Programmer")
+    puts ("Hello #{name}!")
+  end 
+  
